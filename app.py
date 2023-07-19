99cffc8025f678756176c092e2c434c703f1c7e5
Costantino <106625772+c0sta-7@users.noreply.github.com>
2023-07-19 17:44:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 import streamlit as st
 
-st.title('Hello PEDD')
-my_slider = st.slider('slider', 0, 100)
-st.write(my_slider)
 
+st.title('show N tell')
 
-col1, col2, col3 = st.columns([1,1,1])
-with col2:
-    st.write('center')
-
-with st.button('click me'):
+with st.button('Click me'):
     st.ballons()
-
